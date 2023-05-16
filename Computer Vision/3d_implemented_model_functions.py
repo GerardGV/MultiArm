6becfc46ef13158d493435ca10c784bb8c42875a
PolKinsa <1605612@uab.cat>
2023-05-16 16:23:16 +0200

    Renamed the images to get the properly order and modified some code to try to mesh a texture and display it as a 3d model.

--- Computer Vision/3d_implemented_model_functions.py
+++ Computer Vision/3d_implemented_model_functions.py
@@ -3,9 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-
+import open3d as o3d
 import plotly.graph_objects as go
-
 
 
 def show_image(img, title="Imatge", color=False):
@@ -98,8 +97,8 @@ def brute_force_SIFT(im1, im2):
 
 def fundamental_matrix_find_kp_and_match(img1, img2):
     # Implementació més a força bruta i que no trobarà els millors resultats.
-    #img1 = cv2.imread(im_name1, 0)
-    #img2 = cv2.imread(im_name2, 0)
+    # img1 = cv2.imread(im_name1, 0)
+    # img2 = cv2.imread(im_name2, 0)
 
     sift = cv2.SIFT_create()
 
@@ -128,8 +127,8 @@ def fundamental_matrix_find_kp_and_match(img1, img2):
 
 def drawlines(img1, img2, lines, pts1, pts2):
     r, c, _ = img1.shape
-    #img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
-    #img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
+    # img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
+    # img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
 
     for r, pt1, pt2 in zip(lines, pts1, pts2):
         color = tuple(np.random.randint(0, 255, 3).tolist())
@@ -139,6 +138,7 @@ def drawlines(img1, img2, lines, pts1, pts2):
         img1 = cv2.circle(img1, tuple(pt1), 5, color, -1)
         img2 = cv2.circle(img2, tuple(pt2), 5, color, -1)
     return img1, img2
+
 
 # =================================================
 #         ESTIMATE THE FUNDAMENTAL MATRIX
@@ -168,11 +168,14 @@ def draw_epipolar_lines(img1, img2, pts1, pts2, F, mask):
     # Plot the images:
     final_frame = cv2.hconcat((img3, img5))
     return final_frame
+
+
 def least_median_squares_estimation(img1, img2, pts1, pts2):
     F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
 
     final_frame_LMS = draw_epipolar_lines(img1, img2, pts1, pts2, F, mask)
     show_image(final_frame_LMS, "Least Median Squares Image: ")
+
 
 def ransac_estimation(img1, img2, pts1, pts2):
     F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.RANSAC)
@@ -192,9 +195,9 @@ def extractCameraPoses(E):
     u, d, v = np.linalg.svd(E)
     W = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
 
-    Rs, Cs = np.zeros((4,3,3)), np.zeros((4,3))
+    Rs, Cs = np.zeros((4, 3, 3)), np.zeros((4, 3))
 
-    t = u[:,-1]
+    t = u[:, -1]
     R1 = u.dot(W.dot(v))
     R2 = u.dot(W.T.dot(v))
 
@@ -205,8 +208,9 @@ def extractCameraPoses(E):
         R2 = R2 * - 1
     return R1, R2, t
 
-def plotCamera(R,t,ax,scale=0.5, depth=0.5, faceColor='grey'):
-    C = -t # Camera center (world coordinate system)
+
+def plotCamera(R, t, ax, scale=0.5, depth=0.5, faceColor='grey'):
+    C = -t  # Camera center (world coordinate system)
 
     # Generant els eixos de coordenades de la càmera.
     axes = np.zeros((3, 6))
@@ -234,7 +238,7 @@ def plotCamera(R,t,ax,scale=0.5, depth=0.5, faceColor='grey'):
 
     # Vèrtexs pel polígon que es crearà a continuació.
     vertexs = [[pts[:, 0], pts[:, 1], pts[:, 2]], [pts[:, 0], pts[:, 2], pts[:, -1]],
-                [pts[:, 0], pts[:, -1], pts[:, -2]], [pts[:, 0], pts[:, -2], pts[:, 1]]]
+               [pts[:, 0], pts[:, -1], pts[:, -2]], [pts[:, 0], pts[:, -2], pts[:, 1]]]
 
     # Generant el nou polígon
     ax.add_collection3d(Poly3DCollection(vertexs, facecolors=faceColor, linewidths=1, edgecolors='k', alpha=.25))
@@ -250,7 +254,7 @@ def full_ransac_estimation(img1, img2):
 def camera_internals_if_we_DONT_know_K(img1):
     width, height, _ = img1.shape
     focal_length = np.maximum(width, height)
-    center = (height/2, width/2)
+    center = (height / 2, width / 2)
 
     K = np.array(
         [[focal_length, 0, center[0]],
@@ -272,27 +276,25 @@ def estimate_camera_pose_and_draw(img1, img2):
 
     R1, R2, t = extractCameraPoses(E)
     t = t[:, np.newaxis]
-  
+
     # Variable axs per múltiples eixos:
     fig, axs = plt.subplots(2, 2, figsize=(20, 15))
     count = 1
     for i, R_ in enumerate([R1, R2]):
         for j, t_ in enumerate([t, -t]):
-
             axs[i, j] = fig.add_subplot(2, 2, count, projection='3d')
             axs[i, j].set_xlabel('X')
             axs[i, j].set_ylabel('Y')
             axs[i, j].set_zlabel('Z')
             axs[i, j].set_title('Configuració: ' + str(count))
 
-            plotCamera(np.eye(3, 3), np.zeros((3, )), axs[i, j])
+            plotCamera(np.eye(3, 3), np.zeros((3,)), axs[i, j])
             plotCamera(R_, t_[:, 0], axs[i, j])
             count += 1
     return E, pts1, pts2, K
 
 
 def checkForCheiralityCondition(E, pts1, pts2, K):
-
     _, R, t, mask = cv2.recoverPose(E, pts1, pts2, K)
     fig = plt.figure(figsize=(9, 6))
     ax = fig.add_subplot(111, projection='3d')
@@ -328,10 +330,10 @@ def firstReconstruction(pts1, pts2, K, R, t):
     pts3d = getTriangulatedPoints(pts1, pts2, K, R, t, cv2.triangulatePoints)
 
     fig = go.Figure(data=[go.Scatter3d(x=pts3d[:, 0], y=pts3d[:, 1], z=pts3d[:, 2], mode='markers', marker=dict(
-            size=2,
-            color='red',
-            opacity=0.8
-        ))])
+        size=2,
+        color='red',
+        opacity=0.8
+    ))])
     fig.update_layout(autosize=False, width=900, height=900)
     fig.show()
 
@@ -347,16 +349,101 @@ def getTriangulatedPoints(img1pts, img2pts, K, R, t, triangulateFunc):
     img1ptsNorm = cv2.convertPointsFromHomogeneous(img1ptsNorm)[:, 0, :]
     img2ptsNorm = cv2.convertPointsFromHomogeneous(img2ptsNorm)[:, 0, :]
 
-    pts4d = triangulateFunc(np.eye(3, 4), np.hstack((R,t)), img1ptsNorm.T, img2ptsNorm.T)
+    pts4d = triangulateFunc(np.eye(3, 4), np.hstack((R, t)), img1ptsNorm.T, img2ptsNorm.T)
     pts3d = cv2.convertPointsFromHomogeneous(pts4d.T)[:, 0, :]
 
     return pts3d
 
+
 # ===================================
 #       BUNDLE ADJUSTMENT
 # ===================================
+def texture_mapping_sift(model_3d, matches, keypoints, texture_images):
+    # Crear una malla 3D a partir del modelo 3D reconstruido
+    vertices = np.array(model_3d)
+    triangles = np.array([[0, 1, 2]])  # Define los índices de los vértices que forman los triángulos
+    mesh = o3d.geometry.TriangleMesh()
+    mesh.vertices = o3d.utility.Vector3dVector(vertices)
+    mesh.triangles = o3d.utility.Vector3iVector(triangles)
+
+    # Convertir mesh.vertices a un array NumPy para obtener las dimensiones
+    vertices_np = np.asarray(mesh.vertices)
+    num_vertices = vertices_np.shape[0]
+
+    # Crear la textura de la malla utilizando las imágenes de textura
+    texture_coords = []
+    for match in matches:
+        query_idx = match.queryIdx
+        train_idx = match.trainIdx
+        point_2d = keypoints[0][query_idx].pt
+        texture_coords.append([point_2d[0], point_2d[1]])
+    texture_coords = np.array(texture_coords)
+
+    # Alinear las coordenadas de textura al rango [0, 1]
+    texture_coords[:, 0] /= max(texture_images[0].shape[1], texture_images[1].shape[1])
+    texture_coords[:, 1] /= max(texture_images[0].shape[0], texture_images[1].shape[0])
+
+    # Crear una imagen de textura combinando las imágenes de textura
+    texture = np.zeros((num_vertices, 1, 3), dtype=np.uint8)
+    for match in matches:
+        query_idx = match.queryIdx
+        train_idx = match.trainIdx
+        point_3d = int(round(match.distance))  # Utiliza la distancia del match como índice 3D
+        if point_3d > texture.shape[0]:
+            continue
+        point_2d = keypoints[0][query_idx].pt
+        image_index = 0 if point_2d[0] < texture_images[0].shape[1] else 1
+        texture[point_3d, 0] = cv2.cvtColor(texture_images[image_index][int(point_2d[1]), int(point_2d[0])], cv2.COLOR_BGR2RGB)
+
+    # Asignar la textura a la malla
+    mesh.texture = o3d.geometry.Image(texture)
+
+    # Crear el visor 3D para visualizar el modelo texturizado
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis.create_window()
+    vis.add_geometry(mesh)
+    vis.run()
+    vis.destroy_window()
 
 
+
+def TEMP_fundamental_matrix_find_kp_and_match(img1, img2):
+    # Implementació més a força bruta i que no trobarà els millors resultats.
+    # img1 = cv2.imread(im_name1, 0)
+    # img2 = cv2.imread(im_name2, 0)
+
+    sift = cv2.SIFT_create()
+
+    # Find Keypoints and descriptors with SIFT
+    keypt1, descr1 = sift.detectAndCompute(img1, None)
+    keypt2, descr2 = sift.detectAndCompute(img2, None)
+
+    # FLANN parameters:
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(descr1, descr2, k=2)
+    pts1 = []
+    pts2 = []
+    matchesMini = []
+    # Ratio test as per Lowe's paper:
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.8 * n.distance:
+            pts2.append(keypt2[m.trainIdx].pt)
+            pts1.append(keypt1[m.queryIdx].pt)
+            matchesMini.append(m)
+            matchesMini.append(n)
+    pts1 = np.int32(pts1)
+    pts2 = np.int32(pts2)
+    return pts1, pts2, matches, [keypt1,keypt2], matchesMini
+
+def convert_matches_to_list(matches):
+    matches_list = []
+    for match_tuple in matches:
+        for match in match_tuple:
+            matches_list.append(cv2.DMatch(match.queryIdx, match.trainIdx, match.distance))
+    return matches_list
 if __name__ == '__main__':
     print('Starting...')
     folder = 'img/base/'
@@ -367,7 +454,7 @@ if __name__ == '__main__':
     image_names = sorted(image_names)
     print(image_names)
     # Use only the new images:
-    image_names = image_names[6:]
+    image_names = image_names[18:]
     print("New images that will be used in this execution: ", image_names)
     # ================================
     #       LOAD AND SHOW IMAGES
@@ -382,8 +469,21 @@ if __name__ == '__main__':
         # pts1, pts2 = fundamental_matrix_find_kp_and_match(folder + im1_name, folder + im2_name)
         # least_median_squares_estimation(img1, img2, pts1, pts2)
         # ransac_estimation(img1, img2, pts1, pts2)
+
+
+        #E, pts1, pts2, K = estimate_camera_pose_and_draw(img1, img2)
+        #R, t = checkForCheiralityCondition(E, pts1, pts2, K)
+        #firstReconstruction(pts1, pts2, K, R, t)
+
         E, pts1, pts2, K = estimate_camera_pose_and_draw(img1, img2)
         R, t = checkForCheiralityCondition(E, pts1, pts2, K)
-        firstReconstruction(pts1, pts2, K, R, t)
+        pts3d = getTriangulatedPoints(pts1, pts2, K, R, t, cv2.triangulatePoints)
+
+        pts1TEMP, pts2TEMP, matches, keypoints, matchesMini = TEMP_fundamental_matrix_find_kp_and_match(img1, img2)
+        texture_images = [img1, img2]
+        # matchList = convert_matches_to_list(matches)
+        texture_mapping_sift(pts3d, matchesMini, keypoints, texture_images)
+
+
 
     print("End of the program.")
