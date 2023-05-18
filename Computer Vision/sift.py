eeae539373efedb7f0c4a46551612f113db3555a
gerard <1605947@uab.cat>
2023-05-19 01:38:23 +0200

    determinateKeypoints now returns diffferent data structure but it needs to be modify in thw way DoG are stored. assignOrientation function hsa first version of exploring neighbours code, it still needs descripotrs contructors

--- Computer Vision/sift.py
+++ Computer Vision/sift.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
-import scipy
+import math
 import os
+import statistics
 
 
 def load_images_from_folder(folder):
@@ -53,13 +54,49 @@ def filtrarKeypoints(keyPoints:list, DoG:list):
 
     pass
 
-def assignOrientation(keyPoints:list, DoG:list, radi=2):
+#ESTRUCTURAS
+#keyPoints: list[capaDog, tupla(coordenadas)]
+#scales: list[sigma, imagenSuavizada]
+#DoG: list[dict{'D':matrizDoG, 'scales':tupla(indicesScalesProgenitoras)}]
+def assignOrientation(keyPoints:list, scales:list, DoG:list, num_bins=36):
 
-    orientacio=[]
+    scale = 1
 
+    #calculo de magnitudes y orientaciones
     for infoKeypoint in keyPoints:
-        for i in range(0, radi):
 
+        # creamos un numpy array con 36 ellementos, 1 por cada bin, cada bin por cada 10 grados
+        orientation_histogram = np.zeros(num_bins)
+
+        #NO HACE FALTA GUARDAR LA TUPLA DE LAS SCALES GENERADORAS DE LA DoG, en su lugar guardar en lista de keypoints el scale al que pertenece, no el DoG
+
+        #si el valor del keypoint es superior que los valores del mismo en DoG superior y inferior, escogemos la imagen suavizada, scale con k inferior
+        #if DoG[infoKeypoint[0]]['D'][infoKeypoint[1][0]][infoKeypoint[1][1]] == max(DoG[infoKeypoint[0]]['D'][infoKeypoint[1][0]][infoKeypoint[1][1]], DoG[infoKeypoint[0]-1]['D'][infoKeypoint[1][0]][infoKeypoint[1][1]], DoG[infoKeypoint[0]+1]['D'][infoKeypoint[1][0]][infoKeypoint[1][1]]):
+            #scale=1
+        #else:
+            #scale=0
+
+        #caclulode vecinos que observar segun la scala del keypoint, 1.5 veces sigma de la scale
+        radio=int(1.5*infoKeypoint[0])
+
+        for i in range(infoKeypoint[1][1]-radio, infoKeypoint[1][1]+radio):
+            for j in range(infoKeypoint[1][1]-radio, infoKeypoint[1][1]+radio):
+
+                magnitud=math.sqrt(pow(scales[DoG[infoKeypoint[0]]['scales'][scale]][1][infoKeypoint[1][0]+1][infoKeypoint[1][1]]-scales[DoG[infoKeypoint[0]]['scales'][scale]][1][infoKeypoint[1][0]-1][infoKeypoint[1][1]], 2) + pow(scales[DoG[infoKeypoint[0]]['scales'][scale]][1][infoKeypoint[1][0]][infoKeypoint[1][1]+1]-scales[DoG[infoKeypoint[0]]['scales'][scale]][1][infoKeypoint[1][0]][infoKeypoint[1][1]-1], 2))
+                orientacio=math.atan(pow(scales[DoG[infoKeypoint[0]]['scales'][scale]][1][infoKeypoint[1][0]][infoKeypoint[1][1]+1]-scales[DoG[infoKeypoint[0]]['scales'][scale]][1][infoKeypoint[1][0]][infoKeypoint[1][1]-1], 2) / pow(scales[DoG[infoKeypoint[0]]['scales'][scale]][1][infoKeypoint[1][0]+1][infoKeypoint[1][1]]-scales[DoG[infoKeypoint[0]]['scales'][scale]][1][infoKeypoint[1][0]-1][infoKeypoint[1][1]], 2))
+
+                #ponemos valor absoluto a la orientacion en caso de que de un angulo negativo y dividimos entre el numero de bins para que no salga de este rango
+                bin_index = int(abs(orientacio) / (360 / num_bins))
+
+                #añadimos la orientacion ponderada segun la media de la sigma de la scale de esta y su magnitud
+                orientation_histogram[bin_index] += statistics.mean(1.5 * scales[DoG['scales'][scale]][0], magnitud) * orientacio
+
+        plt.hist(orientation_histogram, bins=num_bins)
+        plt.show()
+
+        #creacion nuevo keypoint con la orientacion que en el histograma tiene mas de 80%, mismas coordenadas, diferente angulo
+
+        #creacion descriptor
 
     pass
 def determinateKeypoints(img:np.array, k:float, mode:str, max_scale:int, num_ocatavas:int, kernelSize:int):
@@ -68,26 +105,28 @@ def determinateKeypoints(img:np.array, k:float, mode:str, max_scale:int, num_oca
 
     # Obtenemos la Gaussiana con sigma inicial de 0.7071876 por el video
     sigmaInicial=0.7071876
-
+    nuevaSigma=0.7071876
     # generamos Gausian KERNEL de 1 dimension
     G = cv2.getGaussianKernel(kernelSize, sigmaInicial)
     G = np.outer(G, G)  # genera Gaussian Kernel de 2 dimensiones
 
     DoG = []
-
+    scales = []
     #creamos diferentes octavas con diferentes scales para obtener las diferencias de Gaussianas
     for ocatavActual in range(num_ocatavas):
 
         # convolucionamos, -1 indica queremos un resultado del mismo tamaño de la imagen
         L = cv2.filter2D(imgCopy, -1, G)
 
+        # guardamos la imagent suavizada, L, con sus sigma para luego calcular orientacion
+        scales.append((nuevaSigma, L))
+
         # nueva sigma, se pone +1 porque se necesita el numeron de ocatavas, en octava 0 llevamos 1 octava
         nuevaSigma = pow(k, ocatavActual+1) * sigmaInicial
 
-        # Calculamos octavas hasta el maximo especificado en el hyperparametro
+        # Calculamos escalas hasta el maximo especificado en el hyperparametro
         for scale in range(max_scale):
-            #amplitud o desviacion tipica del filtro Gaussiano
-            #k = pow(2, 1 / scale)
+
 
             #guardamos la convolucion anterior
             Lant = L
@@ -98,15 +137,18 @@ def determinateKeypoints(img:np.array, k:float, mode:str, max_scale:int, num_oca
 
             # convolucionamos, -1 indica que queremos un resultado del mismo tamaño de la imagen
             L = cv2.filter2D(imgCopy, -1, G)
+            scales.append((nuevaSigma, L))
 
             #calculo diferencia de Gaussianas
             D = L-Lant
 
-            #guardamos las octavas para luego sacar los keypoints
-            DoG.append(D)
+            #guardamos las octavas para luego sacar los keypoints y los indices de las escalas de donde las obtenemos
+            DoG.append({'D': D, 'scales': (scale, scale+1)})
 
             #nueva sigma, se pone +1 porque se necesita el numeron de scales, en scale 0 llevamos 1 scale
             nuevaSigma *= pow(k, scale+1)
+
+            scales.append((nuevaSigma, L))
 
         #se reduce la resolucion a la mitad de la octava anterior, para esto se necesita mantener la informacion original, img
         #se subtituye los valores de los pixeles de n columnas, cada octava tiene la mitad de innfromacion de la aterior,
@@ -114,55 +156,53 @@ def determinateKeypoints(img:np.array, k:float, mode:str, max_scale:int, num_oca
         imgCopy[:, 1::pow(2, ocatavActual+1)]=img[:, 0::pow(2, ocatavActual+1)]
 
 
-
-
     keyPoints=[]
 
     #ahora iremos mirando los puntos caracteristicos de cada capa segun su capa anterior y posterir, por ello no miramos la primera ni la ultima
     for capaDoG in range(1, len(DoG)-1):
         #iteramos pixel a pixel miranod 3(x)x3(y)x3(capa, z)-1(pixel que escogemos para mirar a su alrededor) pixeles
-        for i in range(1, DoG[capaDoG].shape[0]-1):
-            for j in range(1, DoG[capaDoG].shape[1]-1):
+        for i in range(1, DoG[capaDoG]['D'].shape[0]-1):
+            for j in range(1, DoG[capaDoG]['D'].shape[1]-1):
 
                 #los prints son TEMPORALES PORQUE LOS IF SE UNIRAN EN UNO UNA VEZ COMPROBADO QUE VAN todos los condicionales max y min se uniran en uno max y otro min
 
-                subMatrizActual=DoG[capaDoG][i-1:i+2, j-1:j+2]
+                subMatrizActual=DoG[capaDoG]['D'][i-1:i+2, j-1:j+2]
                 #print(subMatrizActual)
-                subMatrizAnterior = DoG[capaDoG - 1][i - 1:i + 2, j - 1:j + 2]
+                subMatrizAnterior = DoG[capaDoG - 1]['D'][i - 1:i + 2, j - 1:j + 2]
                # print(subMatrizAnterior)
-                subMatrizPosterior = DoG[capaDoG + 1][i - 1:i + 2, j - 1:j + 2]
+                subMatrizPosterior = DoG[capaDoG + 1]['D'][i - 1:i + 2, j - 1:j + 2]
                 #print(subMatrizPosterior)
 
                 #si el pixel es el maximo o el minimo de su alrededor en la capa o SCALA ACTUAL
-                if DoG[capaDoG][i,j] != 0.0 and max(np.max(subMatrizActual), np.max(subMatrizAnterior),np.max(subMatrizPosterior)) == DoG[capaDoG][i,j]:
+                if DoG[capaDoG]['D'][i, j] != 0 and max(np.max(subMatrizActual), np.max(subMatrizAnterior),np.max(subMatrizPosterior)) == DoG[capaDoG]['D'][i,j]:
                     #concatenar las submatrizes
                     subMatriz = np.concatenate((subMatrizActual, subMatrizAnterior, subMatrizPosterior), axis=0)
 
                     #si el pixel es un maximo unico, no aparace el valor de nuevo en la submatriz, se considera key point
-                    if np.count_nonzero(subMatriz == DoG[capaDoG][i,j]) == 1:
+                    if np.count_nonzero(subMatriz == DoG[capaDoG]['D'][i,j]) == 1:
                         keyPoints.append((capaDoG, (i, j)))#añadimos en que scala de DoG se encuntra para luego las orientaciones
 
-                elif DoG[capaDoG][i,j] != 0.0 and min(np.min(subMatrizActual), np.min(subMatrizAnterior), np.min(subMatrizPosterior)) == DoG[capaDoG][i, j]:
+                elif DoG[capaDoG]['D'][i, j] != 0 and min(np.min(subMatrizActual), np.min(subMatrizAnterior), np.min(subMatrizPosterior)) == DoG[capaDoG]['D'][i, j]:
                     # concatenar las submatrizes
                     subMatriz = np.concatenate((subMatrizActual, subMatrizAnterior, subMatrizPosterior), axis=0)
 
                     # si el pixel es un maximo unico, no aparace el valor de nuevo en la submatriz, se considera key point
-                    if np.count_nonzero(subMatriz == DoG[capaDoG][i, j]) == 1:
+                    if np.count_nonzero(subMatriz == DoG[capaDoG]['D'][i, j]) == 1:
                         keyPoints.append((capaDoG, (i, j)))  # añadimos en que scala de DoG se encuntra para luego las orientaciones
 
-    return keyPoints, DoG
+    return keyPoints, DoG, scales
 
 
 def sift(img: np.array, k=1.6, mode='same', max_scale=3, num_ocatavas=4, kernelSize=3):
     # Step 1: Approximate Keypoint Location
 
-    keyPoints, DoG = determinateKeypoints(img, k, mode, max_scale, num_ocatavas, kernelSize)
+    keyPoints, DoG, scales = determinateKeypoints(img, k, mode, max_scale, num_ocatavas, kernelSize)
 
     #reducir los outliers en los keypoints
     #keypoints = filtrarKeypoints(keyPoints, DoG)
 
     #asignar orientacion a los keypoints
-    assignOrientation(keyPoints, DoG)
+    assignOrientation(keyPoints, scales, DoG)
 
     return 0
 
