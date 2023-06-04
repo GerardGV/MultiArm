3db39a9f9587067ca9e2732161b97290c5614140
gerard <1605947@uab.cat>
2023-06-04 21:55:57 +0200

    clientRobot.py with instruccion to control arduino and raspberry camera

--- cloud/clientRobot.py
+++ cloud/clientRobot.py
@@ -3,10 +3,26 @@ import json
 from google.cloud import storage
 from google.oauth2 import service_account
 from google.auth.transport.requests import AuthorizedSession
+import time
+import picamera
+import serial
+
+puerto = '/dev/ttyACM0'
+velocity = 9600
 
 PORT = 3400
 IP = '34.172.166.240'
 
+def capture_image(file_name):
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.start_preview()
+        time.sleep(2)
+        camera.capture(file_name)
+        camera.stop_preview()
+        capture_image('image1.jpg')
+        time.sleep(5)
+        capture_image('image2.jpg')
 
 def upload_image(bucket_name, file_path, destination_blob_name, credentials_file):
     # Crea una instancia del cliente de Google Cloud Storage con las credenciales de la identidad de servicio
@@ -57,6 +73,11 @@ def communicationRobot(socketUser):
     """
      instruction: It could NAN, TOOLCHG, MOVE, PHOTO, TURN_OFF
     """
+
+    # Inicialize arduino raspberry comunication
+    arduino = serial.Serial(puerto, velocity)
+    time.sleep(1)
+
     # It's listening until recive data from server
     while True:
         serverData = socketUser.recv(1024)
@@ -75,6 +96,11 @@ def communicationRobot(socketUser):
                 # sending data through TCP socket connection
                 socketUser.send(json_data.encode())  # encode transform data to binary
 
+                # iteration of different draw points
+                for point in json_data["message"]:
+                    for axis in point:
+                        arduino.write(axis.encode()) # sending coordates from axis x, y and z
+
             elif instruction == "TOOLCHG":
 
                 json_data = jsonSetUp("NAN", "NAN")
@@ -83,21 +109,26 @@ def communicationRobot(socketUser):
                 # sending data through TCP socket connection
                 socketUser.send(json_data.encode())  # encode transform data to binary
 
+                arduino.write(instruction.encode())# sending signal to change tool
+
             elif instruction == "PHOTO":
+
                 bucket_name = "imagenes-modeling"   # DECLARAMOS EN QUE BUCKET SE SUBEN LAS IMÁGENES
                 credentials_file = "credentials.json"   # USAMOS LAS CREDENCIALES QUE TIENEN PERMISO
 
                 # PARÁMETROS PARA LA PRIMERA IMÁGEN
-                file_path = "imgFaces/img08_V2_1_face_Pol_sense_sostre.jpeg"  # NOMBRE IMAGEN 1
-                destination_blob_name = "image1.jpeg"   # NOMBRE QUE LA IMAGEN QUE TENDRÁ EN EL BUCKET
+                #file_path = "imgFaces/img08_V2_1_face_Pol_sense_sostre.jpeg"  # to test image from local, not raspberry
+                file_path = "image1.jpg"  # path to image1
+                destination_blob_name = "image1.jpeg"   # image name in bucket
 
                 # PARÁMETROS PARA LA SEGUNDA IMÁGEN
-                file_path2 = "imgFaces/img08_V2_2_face_Pol_sense_sostre.jpeg"  # NOMBRE IMAGEN 2
-                destination_blob_name2 = "image2.jpeg"  # NOMBRE QUE LA IMAGEN QUE TENDRÁ EN EL BUCKET
-                # Getting images
+                #file_path2 = "imgFaces/img08_V2_2_face_Pol_sense_sostre.jpeg"  # to test image from local, not raspberry
+                file_path2 = "image2.jpg" # path to image 2
+                destination_blob_name2 = "image2.jpeg"  # image name in bucket
 
-                upload_image(bucket_name, file_path, destination_blob_name, credentials_file)       # IMAGEN 1
-                upload_image(bucket_name, file_path2, destination_blob_name2, credentials_file)     # IMAGEN 2
+                # Getting images
+                upload_image(bucket_name, file_path, destination_blob_name, credentials_file)# sending image 1
+                upload_image(bucket_name, file_path2, destination_blob_name2, credentials_file)# sending image2
 
                 images = ['image1.jpeg', 'image2.jpeg']
 
@@ -111,6 +142,7 @@ def communicationRobot(socketUser):
             elif instruction == "TURN_OFF":
                 # robot turn off
                 socketUser.close()
+                arduino.write(instruction.encode())  # sending signal to change turn off the robot
                 break
 
 
