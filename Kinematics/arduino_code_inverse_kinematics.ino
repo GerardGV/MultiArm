52bc1add9a5513a80b387a05e49d6faebfa62a83
PolKinsa <1605612@uab.cat>
2023-06-04 21:57:55 +0200

    Inverse Kinematics file modified

--- Kinematics/arduino_code_inverse_kinematics.ino
+++ Kinematics/arduino_code_inverse_kinematics.ino
@@ -3,10 +3,21 @@
 #include <Stepper.h>
 
 #define stepsPerRevolution 2048
-#define IN1  10
-#define IN2  11
-#define IN3  12
-#define IN4  13
+
+#define IN1  6
+#define IN2  7
+#define IN3  8
+#define IN4  9
+#define IN5  2
+#define IN6  3
+#define IN7  4
+#define IN8  5
+#define IN9  10
+#define IN10  11
+#define IN11  12
+#define IN12  13
+#define IN13 0
+#define IN14 1
 
 int paso[4][4] =
 {
@@ -31,7 +42,7 @@ double Pi = 3.14159;
 AccelStepper stepper2(AccelStepper::FULL4WIRE, 2, 3, 4, 5);
 AccelStepper stepper1(AccelStepper::FULL4WIRE, 6, 7, 8, 9);
 Stepper stepper3(2048, 10, 11, 12, 13);
-
+char message_raspberry = Serial.read();
 
 double distancia(double x, double y) {
   return sqrt(x * x + y * y);
@@ -85,6 +96,44 @@ void calculate(int x, int y, int z)
   {
     for (int i = 0; i < z; i++)
     {
+      digitalWrite(IN9, paso[i][0]);
+      digitalWrite(IN10, paso[i][1]);
+      digitalWrite(IN11, paso[i][2]);
+      digitalWrite(IN12, paso[i][3]);
+      delay(10);
+    }
+  }
+  delay(1000);
+}
+
+void cabezal()
+{ 
+
+  digitalWrite(IN13, LOW);
+  digitalWrite(IN14, LOW);
+  //subir
+  int pasos1 = 50; // Cantidad de pasos a mover
+
+  for (int j = 0; j < pasos1; j++)
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      digitalWrite(IN9, paso[i][0]);
+      digitalWrite(IN10, paso[i][1]);
+      digitalWrite(IN11, paso[i][2]);
+      digitalWrite(IN12, paso[i][3]);
+      delay(10);
+    }
+  }
+  delay(1000); // Espera 1 segundo antes de reiniciar el movimiento
+
+  //deracha
+  int pasos2 = 280; // Cantidad de pasos a mover
+  
+  for (int j = 0; j < pasos2; j++)
+  {
+    for (int i = 0; i < 4; i++)
+    {
       digitalWrite(IN1, paso[i][0]);
       digitalWrite(IN2, paso[i][1]);
       digitalWrite(IN3, paso[i][2]);
@@ -92,7 +141,87 @@ void calculate(int x, int y, int z)
       delay(10);
     }
   }
+  delay(1000); // Espera 1 segundo antes de reiniciar el movimiento
+
+  //bajar
+  int pasos4 = 60; // Cantidad de pasos a mover
+
+  for (int j = 0; j < pasos4; j++)
+  {
+    for (int i = 3; i >= 0; i--)
+    {
+      digitalWrite(IN9, paso2[i][0]);
+      digitalWrite(IN10, paso2[i][1]);
+      digitalWrite(IN11, paso2[i][2]);
+      digitalWrite(IN12, paso2[i][3]);
+      delay(10);
+    }
+  }
   delay(1000);
+
+
+  //coger cabezal
+  digitalWrite(IN13, HIGH);
+  digitalWrite(IN14, LOW);
+  delay(2000);
+  digitalWrite(IN13, LOW);
+  digitalWrite(IN14, LOW);
+
+  //subir
+  int pasos5 = 60; // Cantidad de pasos a mover
+
+  for (int j = 0; j < pasos5; j++)
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      digitalWrite(IN9, paso[i][0]);
+      digitalWrite(IN10, paso[i][1]);
+      digitalWrite(IN11, paso[i][2]);
+      digitalWrite(IN12, paso[i][3]);
+      delay(10);
+    }
+  }
+  delay(4000); // Espera 1 segundo antes de reiniciar el movimiento
+
+  //bajar
+  int pasos6 = 50; // Cantidad de pasos a mover
+
+  for (int j = 0; j < pasos6; j++)
+  {
+    for (int i = 3; i >= 0; i--)
+    {
+      digitalWrite(IN9, paso2[i][0]);
+      digitalWrite(IN10, paso2[i][1]);
+      digitalWrite(IN11, paso2[i][2]);
+      digitalWrite(IN12, paso2[i][3]);
+      delay(10);
+    }
+  }
+  delay(1000);
+
+  //dejar cabezal
+  digitalWrite(IN13, HIGH);
+  digitalWrite(IN14, HIGH);
+  delay(2000);
+  digitalWrite(IN13, LOW);
+  digitalWrite(IN14, LOW);
+
+  int pasos8 = 60; // Cantidad de pasos a mover
+
+  for (int j = 0; j < pasos8; j++)
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      digitalWrite(IN9, paso[i][0]);
+      digitalWrite(IN10, paso[i][1]);
+      digitalWrite(IN11, paso[i][2]);
+      digitalWrite(IN12, paso[i][3]);
+      delay(10);
+    }
+  }
+  delay(4000);
+
+  exit(0);
 }
 
 void setup() {
@@ -105,34 +234,62 @@ void setup() {
   stepper2.setCurrentPosition(0);
   stepper3.setSpeed(100);
   delay(2000);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  pinMode(IN5, OUTPUT);
+  pinMode(IN6, OUTPUT);
+  pinMode(IN7, OUTPUT);
+  pinMode(IN8, OUTPUT);
+
+  pinMode(IN9, OUTPUT);
+  pinMode(IN10, OUTPUT);
+  pinMode(IN11, OUTPUT);
+  pinMode(IN12, OUTPUT);
+
+  pinMode(IN13, OUTPUT);
+  pinMode(IN14, OUTPUT);
 }
 
 void loop() {
-  serial.print("Introdueix coord X: ");
   while (Serial.available() == 0) {
     // Espera hasta que haya datos disponibles en el búfer de entrada serial
   }
-  float X = Serial.parseFloat();
 
-  serial.print("Introdueix coord Y: ");
-  while (Serial.available() == 0) {
-    // Espera hasta que haya datos disponibles en el búfer de entrada serial
+  if(isdigit(message_raspberry)){
+
+    float X = float(raspberry_message);
+
+    while (Serial.available() == 0) {
+      // Espera hasta que haya datos disponibles en el búfer de entrada serial
+    }
+    float Y = Serial.parseFloat();
+
+    while (Serial.available() == 0) {
+      // Espera hasta que haya datos disponibles en el búfer de entrada serial
+    }
+    float Z = Serial.parseFloat();
+
+    while(Serial.available() == 0){
+      char caracter = Serial.read();
+
+      if (isdigit(caracter)) {
+        numero = numero * 10 + (caracter - '0');
+    }
+
+    calculate(X, Y, Z);
+    delay(1000);
   }
-  float Y = Serial.parseFloat();
-
-  serial.print("Introdueix coord Z: ");
-  while (Serial.available() == 0) {
-    // Espera hasta que haya datos disponibles en el búfer de entrada serial
+  else{
+    if(raspberry_message == "TOOLCHG"){
+      cabezal();
+    }
+    else if(raspberry_message == "TURN_OFF"){
+      exit(0);
+    }
   }
-  float Z = Serial.parseFloat();
-
-  while(Serial.available() == 0){
-    char caracter = Serial.read();
-
-    if (isdigit(caracter)) {
-      numero = numero * 10 + (caracter - '0');
-  }
-
-  calculate(X, Y, Z);
-  delay(1000);
+  
 }
